67f955ca9616355634a5c6b21385e350b65a0567
Muhammad Wachidin <114646249+wahid57@users.noreply.github.com>
2022-09-29 04:01:00 -1200

    Add files via upload

---
+++ soalkedua.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="description" content="BARANG"/>
+        <meta name="Keywords" content="BARANG"/>
+        <meta name="authors" content="Peralatan Gunung"/>
+        <meta charset="UTF-8"/>
+
+        <title>Peralatan Mendaki Gunung Bisa Juga Sewa</title>
+        <link rel="stylesheet" href="cssme.css"/>
+    </head>
+    <body>
+        <form action="#" style="width: 900px"class="posisi";>
+        <fieldset class="h"/>
+        <table style="width: 1200px;">
+            <tr>
+                <td rowspan="10" width="50px">
+                    <p class="judul"> Pesanan Peralatan Mendaki Gunung</p>
+                <img src="cariel.JPG" width="550px" height="550px"/>
+                </td>
+            </tr>
+            <tr>
+                <td><b>Nama Barang</b></td>
+                <td><b>:<b></td>
+                <td>Eiger Wanderlust 28 Gunung Carrier 60L - Red</td>
+            </tr>
+            <tr>
+                <td><b>Kode Barang</b></td>
+                <td><b>:</b></td>
+                <td>Red 91000 44</td>
+            </tr>
+            <tr>
+                <td><b>Spesifikasi Barang</b></td>
+                <td><b>:</b></td>
+                <td>Tas Carrier Eiger Wanderlust 28 Gunung 60L Bag Merah Red 91000 4481<br><br>
+
+                    Fitur:<br>
+                    - Terbuat dari material Cordura yang memiliki ketahanan kuat terhadap tarikan dan abrasi<br>
+                    - Kompartemen utama<br>
+                    - Kompartemen untuk menyimpan kantung air<br>
+                    - Saku sisip<br>
+                    - Kompartemen untuk menyimpan golok<br>
+                    - Dua buah saku samping berbahan mesh<br>
+                    - Dua saku tutup atas<br>
+                    - Tali kompresi samping<br>
+                    - Dua buah pengikat trekking pole<br>
+                    - Saku hip belt<br>
+                    - Cover pelindung hujan<br>
+                    - Fitur reflektif, untuk membuat Anda mudah terlihat dalam keadaan minim cahaya atau malam hari<br>
+                    - Teknologi Aeronodus Backsystem adalah teknologi backsystem yang memberikan kenyamanan saat melakukan<br> 
+                    kegiatan trekking karena menjaga stabilitas beban dan menyediakan sirkulasi udara yang lebih baik.<br>
+                    Dimensi 31x25x80 cm<br>
+                    Bahan Nylon Cordura 420D, Nylon 4<br><br>
+
+                    100% Original<br>
+                    Garansi Resmi 1th (untuk produk TAS Eiger, Exsport, Bodypack)</td>
+            </tr>
+            <tr>
+                <td><b>Harga Barang</b></td>
+                <td><b>:</b></td>
+                <td><b>Rp 2.082.000,-</b></td>
+            </tr>
+            <tr>
+                <td><b>Ekspedisi Pengiriman Barang</b></td>
+                <td><b>:</b></td>
+                <td>JNE, JNT, SiCepat </td>
+            </tr>
+    </table>
+    </fieldset>
+    </form>
+</body>
+</html>
